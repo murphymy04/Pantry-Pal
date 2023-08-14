248797b1820f242e376b651ec0e6fbdfa147aded
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2023-08-14 12:57:15 -0500

    Update server.py

--- api/server.py
+++ api/server.py
@@ -6,12 +6,65 @@ from flask_bootstrap import Bootstrap5
 import werkzeug
 from flask_login import LoginManager, login_user, current_user, UserMixin, logout_user, login_required
 from login_form import LoginForm
+from dotenv import load_dotenv
+import requests
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32)
 Bootstrap5(app)
 
+class RecipeSearch:
+    
+    
+    def __init__(self):
+        load_dotenv()
+        self.RECIPE_ID = os.getenv("recipe_id")
+        self.RECIPE_KEY = os.getenv("recipe_key")
+        self.PARAMS = {
+            "type": "public",
+            "app_id": self.RECIPE_ID,
+            "app_key": self.RECIPE_KEY,
+            "random": True
+        }
+
+
+    def query(self, input):
+        self.PARAMS["q"] = input
+
+    
+    def diet(self, diet_input):
+        self.PARAMS["diet"] = diet_input
+
+    
+    def health(self, health_input):
+        self.PARAMS["health"] = health_input
+
+
+    def cuisine_type(self, cuisine_input):
+        self.PARAMS["cuisineType"] = cuisine_input
+    
+
+    def meal_type(self, meal_input):
+        self.PARAMS["mealType"] = meal_input
+
+    
+    def is_random(self):
+        return self.PARAMS["random"]
+    
+
+    def change_random(self, boolean):
+        self.PARAMS["random"] = boolean
+
+
+    def search(self):
+        self.response = requests.get("https://api.edamam.com/api/recipes/v2", params=self.PARAMS)
+        self.response.raise_for_status()
+        self.data = self.response.json()
+        return self.data
+    
+    def results(self):
+        return self.data
 recipe_search = RecipeSearch()
 
 # setup login manager
